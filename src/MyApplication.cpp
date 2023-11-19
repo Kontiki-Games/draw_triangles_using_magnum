80b8e1eb4d539954fb02d8950363ec6fb569df08
Kontiki-Games <kontiki-games@hotmail.com>
2023-11-19 23:41:04 +0800

    绘制三角形

--- src/MyApplication.cpp
+++ src/MyApplication.cpp
@@ -1,5 +1,9 @@
 #include <Magnum/GL/DefaultFramebuffer.h>
 #include <Magnum/Platform/Sdl2Application.h>
+#include <Magnum/GL/Buffer.h>
+#include <Magnum/GL/Mesh.h>
+#include <Magnum/Math/Color.h>
+#include <Magnum/Shaders/VertexColorGL.h>
 
 using namespace Magnum;
 
@@ -9,16 +13,35 @@ class MyApplication: public Platform::Application {
 
     private:
         void drawEvent() override;
+
+        GL::Mesh _mesh;
+        Shaders::VertexColorGL2D _shader;
 };
 
-MyApplication::MyApplication(const Arguments& arguments): Platform::Application{arguments} {
+using namespace Math::Literals;
+
+struct TriangleVertex {
+    Vector2 position;
+    Color3 color;
+};
+const TriangleVertex vertices[]{
+    {{-0.5f, -0.5f}, 0xff0000_rgbf},    /* Left vertex, red color */
+    {{ 0.5f, -0.5f}, 0x00ff00_rgbf},    /* Right vertex, green color */
+    {{ 0.0f,  0.5f}, 0x0000ff_rgbf}     /* Top vertex, blue color */
+};
+
+MyApplication::MyApplication(const Arguments& arguments): Platform::Application{arguments, Configuration{}.setTitle("Magnum Triangle Example")} {
     /* TODO: Add your initialization code here */
+    _mesh.setCount(Containers::arraySize(vertices))
+        .addVertexBuffer(GL::Buffer{ vertices }, 0,
+            Shaders::VertexColorGL2D::Position{},
+            Shaders::VertexColorGL2D::Color3{});
 }
 
 void MyApplication::drawEvent() {
     GL::defaultFramebuffer.clear(GL::FramebufferClear::Color);
 
-    /* TODO: Add your drawing code here */
+    _shader.draw(_mesh);
 
     swapBuffers();
 }
